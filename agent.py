1cfef5ad757242e157995d41f638cf22cf069ade
Vasu Cho <96566946+chovs@users.noreply.github.com>
2025-04-21 00:17:15 +0700

    Update agent.py

--- agent.py
+++ agent.py
@@ -7,7 +7,7 @@ import asyncio
 import time
 
 initial_actions = [
-    {'open_tab': {'url': 'https://v0-captcha-test-page-yuk7ve.vercel.app/'}}
+    {'open_tab': {'url': 'yourpath/captcha-slider.html'}}
 ]
 
 llm = ChatDeepSeek(model="deepseek-reasoner")
